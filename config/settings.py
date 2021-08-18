886370faa1869917769984b75c373e5ee0d326b2
ggarcia <ggg0830@gmail.com>
2021-08-18 16:50:26 -0500

    Come on

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_on_heroku
+import django_heroku
 from pathlib import Path
 import os
 
@@ -134,4 +134,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
